3d26330b11c4b78a4652ab88376429c4dc7ca7b6
vitorpy <vitorpy@users.noreply.github.com>
2025-10-08 19:44:45 +0200

    Add nvim ACP support via codecompanion.nvim and improve terminal keybindings

--- private_dot_config/nvim/lua/plugins/toggleterm.lua
+++ private_dot_config/nvim/lua/plugins/toggleterm.lua
@@ -20,8 +20,62 @@ return {
       },
     })
 
-    -- Custom terminal keybindings
-    vim.keymap.set("n", "<leader>t", ":ToggleTerm<CR>", { desc = "Toggle terminal" })
+    local Terminal = require("toggleterm.terminal").Terminal
+
+    -- Track current terminal index
+    local term_idx = 1
+    local terminals = {}
+
+    -- Function to get or create terminal
+    local function get_terminal(idx)
+      if not terminals[idx] then
+        terminals[idx] = Terminal:new({
+          count = idx,
+          direction = "float",
+          on_close = function()
+            terminals[idx] = nil
+          end,
+        })
+      end
+      return terminals[idx]
+    end
+
+    -- Toggle current terminal
+    vim.keymap.set("n", "<leader>t", function()
+      get_terminal(term_idx):toggle()
+    end, { desc = "Toggle terminal" })
+
+    -- Create new terminal (next tab)
+    vim.keymap.set("n", "<leader>tn", function()
+      term_idx = term_idx + 1
+      get_terminal(term_idx):toggle()
+    end, { desc = "New terminal tab" })
+
+    -- Cycle to next terminal
+    vim.keymap.set("n", "<leader>]", function()
+      get_terminal(term_idx):close()
+      term_idx = term_idx % 10 + 1
+      get_terminal(term_idx):open()
+    end, { desc = "Next terminal" })
+
+    -- Cycle to previous terminal
+    vim.keymap.set("n", "<leader>[", function()
+      get_terminal(term_idx):close()
+      term_idx = term_idx - 1
+      if term_idx < 1 then term_idx = 10 end
+      get_terminal(term_idx):open()
+    end, { desc = "Previous terminal" })
+
+    -- Close current terminal
+    vim.keymap.set("n", "<leader>tc", function()
+      get_terminal(term_idx):shutdown()
+      terminals[term_idx] = nil
+    end, { desc = "Close terminal" })
+
+    -- Exit terminal mode
     vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], { desc = "Exit terminal mode" })
+
+    -- Pass C-c to terminal
+    vim.keymap.set("t", "<C-c>", "<C-c>", { desc = "Send C-c to terminal" })
   end,
 }

c2589aa23180d2c68aaabb0bfd24b554b50baaf0
Vitor Py <vitor@vitorpy.com>
2026-04-23 20:41:23 +0200

    Fix mediaserver unattended installer bootstrap

--- private_dot_config/private_arch/archiso/airootfs/root/executable_install-chroot.sh
+++ private_dot_config/private_arch/archiso/airootfs/root/executable_install-chroot.sh
@@ -11,6 +11,12 @@ locale-gen
 useradd -m -G wheel -s /usr/bin/zsh "$USERNAME"
 passwd -l "$USERNAME"
 
+cat > /etc/sudoers.d/99-vitorpy-bootstrap <<EOF
+${USERNAME} ALL=(ALL:ALL) NOPASSWD: ALL
+EOF
+chmod 440 /etc/sudoers.d/99-vitorpy-bootstrap
+visudo -cf /etc/sudoers.d/99-vitorpy-bootstrap
+
 install -d -m 700 /home/"$USERNAME"/.ssh
 cat > /home/"$USERNAME"/.ssh/authorized_keys <<'KEYS'
 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEjbmkLFQ7T6sLQ9DaeNeW8KB42RjHBvIowNz892tJN5 vitorpy@zygalski

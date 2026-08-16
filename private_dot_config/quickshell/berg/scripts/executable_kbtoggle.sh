97cf5b241b3ff872f6a64e762bc3e62d2164f50e
Vitor Py <vitor@vitorpy.com>
2026-08-16 17:37:19 +0200

    Retire Waybar

--- private_dot_config/quickshell/berg/scripts/executable_kbtoggle.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-KB="$(/usr/bin/hyprctl devices -j | /usr/bin/jq -r \
-  '.keyboards[] | select(.main==true) | .name' | head -n1)"
-exec /usr/bin/hyprctl switchxkblayout "$KB" next
--- private_dot_config/waybar/scripts/executable_brightness.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Get current and max brightness
-CURRENT=$(brightnessctl get)
-MAX=$(brightnessctl max)
-# Calculate percentage
-BRIGHTNESS=$((CURRENT * 100 / MAX))
-printf '\U001001AE %s%%\n' "$BRIGHTNESS"
